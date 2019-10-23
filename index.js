e748ddfc89822b3753310d6b9ec62f142f47fb4f
Daniel Dihardja <daniel@dihardja.de>
2019-10-23 20:26:29 +0200

    added offline model

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const config = {
   kw: [... Array(88).keys()],
   tempr: 0.5,
   lowestMidiNote : 21,
-  genieCheckpoint : 'https://storage.googleapis.com/magentadata/js/checkpoints/piano_genie/model/epiano/stp_iq_auto_contour_dt_166006',
+  // genieCheckpoint : 'https://storage.googleapis.com/magentadata/js/checkpoints/piano_genie/model/epiano/stp_iq_auto_contour_dt_166006',
+  genieCheckpoint : './checkpoint',
   midiMapping: {
     48: 0,
     50: 1,
@@ -18,14 +19,13 @@ const config = {
     55: 4,
     57: 5,
     59: 6,
-    60: 7
+    60: 7,
   }
 };
 
 const keyMap = {};
 const keyDown$ = fromEvent(window, 'keydown');
 const keyUp$ = fromEvent(window, 'keyup');
-
 
 const initGenie = (checkpoint, whiteKeys, tempr) => new Observable(observer => {
   const genie = new mm.PianoGenie(checkpoint);
@@ -38,38 +38,38 @@ const initGenie = (checkpoint, whiteKeys, tempr) => new Observable(observer => {
     });
 });
 
-
-
 const initMidi = () => new Observable(observer => {
   navigator.requestMIDIAccess()
   .then(e => {
 
-    let mout = [...e.outputs.values()][0];
-    let min = [...e.inputs.values()][1];
+    const mout = [...e.outputs.values()][0];
+    const min = [...e.inputs.values()][1];
+    let prevMsg = [];
 
     observer.next({
       in: min,
       out: mout,
       msg: []
     });
-    
+
     min.onmidimessage = data => {
       observer.next({
         in: min,
         out: mout,
-        msg: data.data
+        msg: data.data,
+        prevMsg: prevMsg
       });
+      prevMsg = data.data;
     };
   });
 });
-
 
 const magic = e => {
   const cfg = e[0];
   const genie = e[1];
   const msg = e[2].msg;
-  if(msg.length === 0) return;
 
+  if(msg.length === 0) return;
   if (! cfg.midiMapping[msg[1]] && cfg.midiMapping[msg[1]] !== 0) return;
 
   const mout = e[2].out;
